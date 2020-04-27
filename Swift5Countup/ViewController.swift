acf8d3de4b308c523ef9a0e8053422aa3b5cc40b
cozenakai <masaki2970@icloud.com>
2020-04-27 23:58:07 +0900

    count up

--- Swift5Countup/ViewController.swift
+++ Swift5Countup/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var count = 0
+    
     
     @IBOutlet weak var countLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //ラベルの文字を０と表示したい
+        countLabel.text
+        
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        //ラベルに文字を反映したい
+        
+        
+        //カウントダウンしたい
+         
+            count = count+1
+        //色を１０以上になったら帰路にしたい
+        
+        
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    
 }
 
